2ae4eb6724ec2e3ede151662b5ee6d8cefe2ad48
ronya78@gmail.com <ronya78@gmail.com>
2021-11-11 18:23:36 +0200

    Add code

--- index.js
+++ index.js
@@ -12,3 +12,25 @@ $("h1").text("Bye");
 $("button").text("Don't click me"); //Selected all buttons to change
 
 $("button").html("<em>Hey</em>");
+
+//------------Manipulating Attributes with jQuery
+$("img").attr("src");
+
+$("a").attr("href", "https://www.yahoo.com");
+
+//-----------Adding Event Listener with jquery
+$("h1").click(function() {
+  $("h1").css("color", "purple");
+});
+
+//----If we need to change all of set buttons we need to use loop weth JavaScript
+// for ( var i = 0; i < 5; i++){
+//   document.querySelectorAll("button")[i].addEventListener("click", function() {
+//     document.querySelector("h1").style.color = "purple";
+//   });
+// }
+
+//----The same but with using jquery
+$("button").click(function() {
+  $("h1").css("color", "purple");
+});
