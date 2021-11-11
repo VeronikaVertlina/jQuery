dd46aa1b875b437877359a77296cc177b4928cf4
ronya78@gmail.com <ronya78@gmail.com>
2021-11-11 16:49:05 +0200

    Add code

--- index.js
+++ index.js
@@ -2,4 +2,13 @@
 
 // $("h1").css("font-size", "5rem");
 
+//--------------Manipulating Styles with jQuery
 $("h1").addClass("big-title margin-50");
+
+
+//-------------Manipulating Text with jQuery
+$("h1").text("Bye");
+
+$("button").text("Don't click me"); //Selected all buttons to change
+
+$("button").html("<em>Hey</em>");
